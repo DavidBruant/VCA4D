63c1b12212a308f6330bfef586d1f0d61a2e19ec
David Bruant <davidbruant@protonmail.com>
2023-03-02 11:38:44 +0100

    Move StudyOverview to its own component and create navigation to StudyEnvironment

---
+++ src/components/StudyEnvironment.vue
@@ -0,0 +1,34 @@
+<template>
+    <article>
+        <h1>Is the value chain environmentally sustainable? </h1>
+        
+
+    </article>
+
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<script>
+//@ts-check
+
+export default {
+    name: 'StudyEnvironment',
+    props: ['studyData'],
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+article {
+
+}
+
+</style>
